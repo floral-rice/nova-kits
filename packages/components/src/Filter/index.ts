56b9c3711b61a43e35cf93dff59c5c3eaa545bbd
zhou <zhouyc@cnlemo.net>
2026-09-01 22:23:58 +0800

    Revert "feat(filter): 添加 Filter 组件"
    This reverts commit c363bb79e261e4ab99fcf7b751a54f8b713b2bdc.

--- packages/components/src/Filter/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as NFilter } from './Filter.vue'
-export { default as NFilterFieldsConfig } from './FilterFieldsConfig.vue'
-export type * from './types'
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -6,18 +6,13 @@ import './styles/_variables.scss';
 import ElementPlus from 'element-plus';
 
 export { NLayoutWithSub as NLayout, NLayoutTabs, NLayoutPortal, useTabFocus } from './Layout/index';
-export { NFilter, NFilterFieldsConfig } from './Filter/index';
-export type * from './Filter/types';
 
 import { NLayoutWithSub as NLayout, NLayoutTabs, NLayoutPortal } from './Layout/index';
-import { NFilter, NFilterFieldsConfig } from './Filter/index';
 
 export function install(app: App) {
   app.component('NLayout', NLayout);
   app.component('NLayoutTabs', NLayoutTabs);
   app.component('NLayoutPortal', NLayoutPortal);
-  app.component('NFilter', NFilter);
-  app.component('NFilterFieldsConfig', NFilterFieldsConfig);
   app.use(ElementPlus);
 }
 
